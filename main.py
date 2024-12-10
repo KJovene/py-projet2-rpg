75220d861ce88d9d3e3b74f0c6eba0be09f82a96
Laurent Dubois <laurent.dubois91710@gmail.com>
2024-12-10 13:46:38 +0100

    confirmation de la méthode turn, lock de self.active.player, modification de add_xp

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ class Combat:
         self.turn_number = 0 
         self.player = player
         self.opponent = opponent
-        self.active_player = 0 # 0 = Monster / 1 = Player
+        self.active_player = randint(0 , 1) # 0 = Player /  1 = Monster // Détermine celui qui commence en premier 
     
     def start(self):
         #Début du combat
-        Console.print("[red]Vous vous apprêtez à vous battre contre {self.opponent}...\n QUE LE COMBAT COMMENCE[/red]")
-        self.active_player = randint(0 , 1) # 0 = Player /  1 = Monster // Détermine celui qui commence en premier       
+        Console.print("[red]Vous vous apprêtez à vous battre contre {self.opponent}...\n QUE LE COMBAT COMMENCE[/red]")    
         while self.player.health != 0 and self.opponent.health != 0: #Boucle des tours
-            Console.print(f"[bold]Vous avez {self.player.health} PDV. \n {self.opponent.name} a {self.opponent.health} PDV.") #Affichage des PDV à chaque début de tour
+            Console.print(f"[bold]Vous avez {self.player.health} PV. \n {self.opponent.name} a {self.opponent.health} PV.") #Affichage des PV à chaque début de tour
             self.turn()
         self.end()
         
@@ -52,8 +51,7 @@ class Combat:
             if player_interact == '1':
                 
                 self.player.attack(self.opponent)                
-                #Console.print(f"Vous infligez {attack_opponent} dégâts à {self.opponent.name} !")
-                Console.print(f"Il reste {self.opponent.health} PDV à {self.opponent.name}.")
+                Console.print(f"Il reste {self.opponent.health} PV à {self.opponent.name}.")
                 
             elif player_interact == '2' :
 
@@ -68,9 +66,9 @@ class Combat:
 
                 if Menu_choice == '1' :
                     self.player.show_inventory() #Ré-affiche l'inventaire
-                    if player.inventory: #Si le joueur possède un inventaire
-                        item_name = Prompt.ask("Entrez le nom de l'objet à utiliser :")
-                        player.use_item(item_name)
+                    if self.player.inventory: #Si le joueur possède un inventaire
+                        item_id = Prompt.ask("Entrez le nom de l'objet à utiliser :", choices = [i for i in range(len(self.player.inventory))])
+                        self.player.use_item(item_id)
                     else:
                         console.print("[yellow]Votre inventaire est vide ![/yellow]")           
 
@@ -83,16 +81,16 @@ class Combat:
             
     def opponent_turn(self):
         
-            attack_player = self.opponent.attack(self.player)
-            #player_damaged = self.player.take_damage(attack_player)
-            Console.print(f"Il vous reste {self.player.health} PDV.")
+            self.opponent.attack(self.player)
+            Console.print(f"Il vous reste {self.player.health} PV.")
 
     def end(self):
         if self.opponent.health <= 0 :
-            amount_xp = self.player.add_xp(150) #Ajoute l'xp au héro, 150 par défault
+            amount_xp = 150 
+            self.player.add_xp(amount_xp) #Ajoute l'xp au héro, 150 par défault
             drop_item = self.opponent.calculate_drops(dropable_items) #Drop du monstre, dropable_items = la liste des drops du monstre
             Console.print("Le combat est terminé !")
-            Console.print("[cyan]Vous avez vaincu {self.opponent.name} \n XP = +{amount_xp} \n Vous avez trouvé {drop_item}[/cyan]")
+            Console.print("[cyan]Vous avez vaincu {self.opponent.name} \n vous avez gagne {amount_xp} xp, il vous manque ... xp pour augmenter de niveau \n Vous avez trouvé {drop_item}[/cyan]")
         
         if self.player.health <= 0 :
             Console.print("[red]Vous avez été vaincu comme un Looser que vous êtes ! Vous retournez au spawn bredouille ![/red]")          
@@ -102,6 +100,3 @@ class Combat:
     def escape(self):
         Console.print("[cyan]Vous arrivez à vous enfuir comme un lâche ![/cyan]")
         self.player.move('spawn')
-        
-
-player = combat(player: Player, opponent: Monster)
